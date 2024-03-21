a33d4a8c7daa3004840f6dd7b7e4c49a98896275
Kiwis01 <68796207+Kiwis01@users.noreply.github.com>
2024-03-20 23:24:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+/* Given the 'id' attribute of a form element, this will return the value entered by the user into that form element. */
+function formValue(id) {
+  // ???
+  let formElement = document.getElementById(id);
+  
+  // ???
+  if (formElement === null) {
+    return `[${id} NOT FOUND]`;
+  }
+
+  let value = formElement.value;
+
+	// ???
+  if (formElement.value.trim() === "") {
+    return `[${formElement.placeholder}]`;
+  }
+
+  return document.getElementById(id).value;
+}
+
+/* Given the 'id' of an HTML element and a 'className', this will add that class name to the HTML element with the specified id. */
+function addClassToElement(id, className) {
+  let element = document.getElementById(id);
+  element.classList.add(className);
+}
+
+/* The code here will be executed each time the Generate button is clicked. */
+function generate() {
+  console.log("button clicked.");
+
+  let relative = formValue("relative");
+  let adjective1 = formValue("adjective-1");
+  let adjective2 = formValue("adjective-2");
+  let adjective3 = formValue("adjective-3");
+  let famousPerson = formValue("Famous-Person");
+  let noun = formValue("noun");
+  let dessert = formValue("dessert");
+  let petName = formValue("pet-name");
+  
+  let madlib = `
+    Dear ${relative},
+    <br><br>
+    I have been having a really ${adjective1} time at camp. The counselour is ${adjective2} and the food is ${adjective3}. I met ${famousPerson} and we quickly became ${noun}. Talk soon!
+    <br><br>
+    Your ${dessert},
+    <br>
+    ${petName}  
+  `;
+
+    document.getElementById("output").innerHTML = madlib;
+    addClassToElement("container", "generated");
+  
+}
